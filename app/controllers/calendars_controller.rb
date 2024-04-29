0cb3c669e9abf0b1d064b7fc835bddc00f589c3d
hayapi0208 <yhayasaska0208@gmail.com>
2024-04-29 16:25:48 +0900

    Issue5,6の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,9 +36,6 @@ class CalendarsController < ApplicationController
       end
 
       wday_num = wdays[(@todays_date + x).wday]
-      if wday_num >= "7"
-        wday_num = wday_num -7
-      end
 
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wday_num }
 
